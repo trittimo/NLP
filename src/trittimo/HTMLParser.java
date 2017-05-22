bc489d5720b012ee7b19000106bb3d055b952bbd
Andrew McKee <mckeeaj@rose-hulman.edu>
2017-05-22 19:30:04 -0400

    Added HTMLParser
    Parses HTML page into relevant plaintext.

---
+++ src/trittimo/HTMLParser.java
@@ -0,0 +1,20 @@
+package trittimo;
+
+public class HTMLParser {
+	/**
+	 * 
+	 * Ignores everything before the </head> tag, then removes all tags from the document. 
+	 * 
+	 * @param html String of HTML Data
+	 */
+	public static String deparse(String html) {
+		final String START_BODY = "</body>";
+		final String EMPTY = "";
+		// skip header metadata
+		String body = html.substring(html.indexOf(START_BODY) + START_BODY.length());
+		// remove tags (this hits comments too)
+		body = body.replaceAll("<.*?>", EMPTY);
+		// return clean
+		return body;
+	}
+}
