b267e360361f6c4bd7431e287fecae893008eb0c
Michael Trittin <trittimo@rose-hulman.edu>
2017-05-22 19:48:28 -0400

    Begin work on preprocessor

--- src/trittimo/NLP.java
+++ src/trittimo/NLP.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
 public class NLP {
 	public static void main(String[] args) throws IOException {
 		if (args.length < 2) {
@@ -26,15 +29,22 @@ public class NLP {
 		for (File f : infolder.listFiles()) {
 			String content = new String(Files.readAllBytes(f.toPath()), StandardCharsets.UTF_8);
 			if (f.getName().endsWith(".html") || f.getName().endsWith(".html")) {
-				// TODO html parsing
+				content = HTMLParser.deparse(content);
 			}
 			rawData.add(new String[] {f.getName(), content});
 		}
 		
-		
-		
 		Properties arguments = new Properties();
 		arguments.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(arguments);
+		
+		for (String[] datum : rawData) {
+			Annotation document = new Annotation(datum[1]);
+			pipeline.annotate(document);
+			System.out.println(document.toString());
+		}
+		
 		
 	}
 }
