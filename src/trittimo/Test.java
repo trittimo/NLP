94858df0925e3559e1ab96885aa1cbb91b250993
Andrew McKee <mckeeaj@rose-hulman.edu>
2017-05-22 22:27:53 -0400

    Example of matching head to other references

--- src/trittimo/Test.java
+++ src/trittimo/Test.java
@@ -1,14 +1,19 @@
 package trittimo;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import edu.stanford.nlp.coref.CorefCoreAnnotations;
 import edu.stanford.nlp.coref.data.CorefChain;
-import edu.stanford.nlp.coref.data.Mention;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.IntPair;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
@@ -16,20 +21,19 @@ public class Test {
 		Annotation document = new Annotation("Barack Obama was born in Hawaii. He is the president of the United States. Obama was elected in 2008.");
 
 		Properties props = new Properties();
-		props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,dcoref");
+		props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,mention,dcoref");
 		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 		pipeline.annotate(document);
 		System.out.println("---");
-		System.out.println("coref chains");
-		for (CorefChain cc : document.get(CorefCoreAnnotations.CorefChainAnnotation.class).values()) {
-			System.out.println("\t"+cc);
+		List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+		Collection<CorefChain> chains = document.get(CorefCoreAnnotations.CorefChainAnnotation.class).values();
+		for (CorefChain chain : chains) {
+			Map<IntPair,Set<CorefChain.CorefMention>> map = chain.getMentionMap();
+			for (IntPair ip : map.keySet()) {
+				for (CorefChain.CorefMention cm : map.get(ip)) {
+					System.out.printf("(%s) %s - Sentence %d%n", chain.getRepresentativeMention().mentionSpan, cm.mentionSpan, cm.position.elems()[0]);
+				}
+			}
 		}
-//		for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
-//			System.out.println("---");
-//			System.out.println("mentions in sentence " + sentence);
-//			for (Mention m : sentence.get(CorefCoreAnnotations.CorefMentionsAnnotation.class)) {
-//				System.out.println("\t"+m);
-//			}
-//		}
 	}
 }
