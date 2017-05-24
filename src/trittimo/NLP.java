7ac70593c9d9b273ce03ddefdcb3fc5bf282dc49
Michael Trittin <trittimo@rose-hulman.edu>
2017-05-23 20:27:23 -0400

    Cleanup

--- src/trittimo/NLP.java
+++ src/trittimo/NLP.java
@@ -2,6 +2,8 @@ package trittimo;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,9 +15,11 @@ public class NLP {
 		if (args.length < 2) {
 			System.err.println("Usgae: NLP <input folder> <search phrases...>");
 			System.err.println("Example: NLP data/presidents \"Lincoln birthday\" \"George Washington Age\"");
-			System.err.println("USAGE: NLP <input folder> -CLI");
+			System.err.println("If you would prefer to parse the documents and enter multiple queries at your leisure");
+			System.err.println("Usage: NLP <input folder> -CLI");
 			return;
 		}
+		
 		File infolder = new File(args[0]);
 		
 		if (!infolder.isDirectory()) {
@@ -24,62 +28,90 @@ public class NLP {
 		}
 		
 		if (args[1].equals("-CLI")) {
-			List<Tuple<String, Annotation>> raw = new ArrayList<>();
-			for (File f : infolder.listFiles()) {
-				System.out.println("Analyzing document: " + f.getName());
-				raw.add(new Tuple<>(f.getName(), Preprocessor.loadFile(f.toPath().toString())));
-			}
-			while (true) {
-				Scanner s = new Scanner(System.in);
-				System.out.println("Type 'exit' to quit"); 
-				System.out.print("Query: ");
-				String query = s.nextLine();
-				if (query.equals("exit")) {
-					return;
-				}
-				for (Tuple<String, Annotation> datum : raw) {
-					System.out.print("Searching " + datum.first + " for phrase '" + query + "': ");
-					if (DocumentSearcher.search(datum.second, Preprocessor.parseQuery(query), 0.5f)) {
-						System.out.println("YES");
-					} else {
-						System.out.println("NO");
-					}
-				}
-			}
+			CLI(infolder);
+		} else {
+			analyze(infolder, args);
 		}
-		
+	}
+
+	private static void analyze(File infolder, String[] args) {
 		List<Tuple<String, Annotation>> queries = new ArrayList<>();
 		for (int i = 1; i < args.length; i++) {
 			System.out.println("Analyzing query: " + args[i]);
+			mute();
 			queries.add(new Tuple<>(args[i], Preprocessor.parseQuery(args[i])));
+			unmute();
 		}
 		
 
 		List<Tuple<String, Annotation>> raw = new ArrayList<>();
 		for (File f : infolder.listFiles()) {
 			System.out.println("Analyzing document: " + f.getName());
+			mute();
 			raw.add(new Tuple<>(f.getName(), Preprocessor.loadFile(f.toPath().toString())));
+			unmute();
 		}
 		
 		
 		for (Tuple<String, Annotation> datum : raw) {
 			for (Tuple<String, Annotation> query : queries) {
 				System.out.print("Searching " + datum.first + " for phrase '" + query.first + "': ");
+				mute();
 				if (DocumentSearcher.search(datum.second, query.second, 0.5f)) {
-					System.out.println("YES");
+					unmute();
+					System.out.println("true => phrase exists");
 				} else {
-					System.out.println("NO");
+					unmute();
+					System.out.println("false => phrase does not exist");
 				}
 			}
 		}
 	}
 	
-
-	public static void mute() {
+	private static void CLI(File infolder) {
+		List<Tuple<String, Annotation>> raw = new ArrayList<>();
+		for (File f : infolder.listFiles()) {
+			System.out.println("Analyzing document: " + f.getName());
+			mute();
+			raw.add(new Tuple<>(f.getName(), Preprocessor.loadFile(f.toPath().toString())));
+			unmute();
+		}
 		
+		Scanner s = new Scanner(System.in);
+		while (true) {
+			System.out.println("Type 'exit' to quit"); 
+			System.out.print("Query: ");
+			String query = s.nextLine();
+			if (query.equals("exit")) {
+				break;
+			}
+			
+			for (Tuple<String, Annotation> datum : raw) {
+				System.out.print("Searching " + datum.first + " for phrase '" + query + "': ");
+				mute();
+				if (DocumentSearcher.search(datum.second, Preprocessor.parseQuery(query), 0.5f)) {
+					unmute();
+					System.out.println("true => phrase exists");
+				} else {
+					unmute();
+					System.out.println("false => phrase does not exist");
+				}
+			}
+		}
+		s.close();
 	}
 	
-	public static void unmute() {
-		
+	private static PrintStream out = System.out;
+	private static PrintStream err = System.err;
+	private static PrintStream silenced = new PrintStream(new OutputStream() {public void write(int b) throws IOException {}});
+	
+	private static void mute() {
+		System.setErr(silenced);
+		System.setOut(silenced);
+	}
+	
+	private static void unmute() {
+		System.setOut(out);
+		System.setErr(err);
 	}
 }
